9cb439c76fca97536a57d1ed7211efeef7990d14
lichun <i@lichun.me>
2018-08-12 16:10:14 +0800

    Remove archive script.

--- .travis/deploy.sh
+++
@@ -1,5 +0,0 @@
-xcodebuild archive -scheme Bootcamp -sdk iphoneos -configuration Release -archivePath build/Bootcamp.xcarchive | xcpretty
-xcodebuild -exportArchive -exportOptionsPlist .travis/ExportOptions.plist  -archivePath build/Bootcamp.xcarchive -exportPath build/
-
-fir login "${FIR_TOKEN}"
-fir publish build/Bootcamp.ipa
